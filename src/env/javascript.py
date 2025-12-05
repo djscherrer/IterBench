4e76bb8a8bf4839e028b9a38ceff86b3757838c5
Nicolas Baeriswyl <nbaeriswyl@ethz.ch>
2025-12-05 09:12:20 +0100

    smaller js docker image

--- src/env/javascript.py
+++ src/env/javascript.py
@@ -6,8 +6,9 @@ _PACKAGE_JSON_FILENAME = "package.json"
 
 _JS_DOCKERFILE = f"""
 # setup base
-FROM node:22.12-bullseye
+FROM node:22.12-bullseye-slim
 RUN apt-get update
+RUN apt-get install procps -y
 RUN mkdir -p {_WORKDIR}
 # WORKDIR has to come first, otherwise npm fails to install packages
 WORKDIR {_WORKDIR}
